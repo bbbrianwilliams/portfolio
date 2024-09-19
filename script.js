2ec167d0fef8233811211089da0a7e38996e0a68
Brian Williams <120751965+bbbrianwilliams@users.noreply.github.com>
2024-09-19 12:23:46 -0400

    added more styles, added dark mode

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const navbar = document.querySelector("[data-navbar]");
 const navTogglerElements = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
-
-
-
+const themeBtn = document.querySelector("#theme-btn");
+const body = document.querySelector("body");
+const sectionText = document.querySelectorAll(".section-text");
+const getAllDividers = document.querySelectorAll(".divider");
+const labelText = document.querySelectorAll(".label-text");
 
 function handleElementsOnClick(elements, eventType, callbackFunc) {
   for (let i = 0; i < elements.length; i++) {
@@ -16,5 +18,33 @@ function handleToggleNavbar() {
   overlay.classList.toggle("active");
 }
 
-handleElementsOnClick(navTogglerElements, 'click', handleToggleNavbar)
+handleElementsOnClick(navTogglerElements, "click", handleToggleNavbar);
 
+themeBtn.addEventListener("click", () => {
+  if (body.getAttribute("class") === "blue") {
+    body.classList.remove("blue");
+    body.classList.add("black");
+
+    getAllDividers.forEach((item) => {
+      item.removeAttribute("data-theme", "blue");
+      item.setAttribute("data-theme", "black");
+    });
+
+    sectionText.forEach((item) => {
+      item.setAttribute("data-theme", "black");
+    });
+  } else if (body.getAttribute("class") === "black") {
+    body.classList.remove("black");
+    body.classList.add("blue");
+
+    getAllDividers.forEach((item) => {
+      item.removeAttribute("data-theme", "black");
+      item.setAttribute("data-theme", "blue");
+    });
+
+    sectionText.forEach((item) => {
+      item.removeAttribute("data-theme", "black");
+      item.setAttribute("data-theme", "blue");
+    });
+  }
+});
